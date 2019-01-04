00e2e4a4396531594c54ad644f80a68b2f44951c
perfectacle <perfectacle@gmail.com>
2019-01-04 19:04:10 +0900

    update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = 3000;
 setTimeout(() => {
   http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
+    res.end('Hello World!');
   }).listen(port);
 }, 30 * 1000);
