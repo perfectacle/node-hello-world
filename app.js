36ff7c01ace53041b509e530b696625825774e6d
perfectacle <perfectacle@gmail.com>
2019-01-04 18:34:14 +0900

    무중단 배포 테스트 해보려고 일부러 30초 이따가 서버 뜨게 설정

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const port = 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
-}).listen(port);
+setTimeout(() => {
+  http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+  }).listen(port);
+}, 30 * 1000);
