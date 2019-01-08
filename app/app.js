b6639daf48ddff35711b09e6ca6974c4e758a8bb
perfectacle <perfectacle@gmail.com>
2019-01-08 23:50:41 +0900

    블루그린 배포 구현

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
 const http = require('http');
-const port = 3000;
 const serverListenHandler = () => http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(port);
+  res.end('Hello World@');
+}).listen(3000);
 
 // 무중단 배포를 확인하기 위해 서버가 뜨는 시간을 딜레이를 줌.
-const delay = 30;
+const delay = 5;
 setTimeout(serverListenHandler, delay * 1000);
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-docker-compose build
+docker-compose build app
 
-docker-compose stop app-a
-docker-compose up -d app-a
+existA=`docker exec node-hello-proxy ls /etc/nginx/conf.d/ | grep proxy-a.conf`
+if [[ ! -z ${existA} ]]; then
+    green="b"
+    blue="a"
+else
+    green="a"
+    blue="b"
+fi
+
+docker-compose stop app-${green}
+docker-compose up -d app-${green}
 
 status=""
 while [[ -z ${status} ]]; do
     echo "health check"
-    status=`curl -s -I localhost:9000 | grep 200`
+    status=`docker exec node-hello-${green} wget --spider -S localhost:3000 2>&1 | grep "HTTP/" | awk '{print $2}'`
     sleep 1
 done
 
-docker-compose stop app-b
-docker-compose up -d app-b
+docker exec node-hello-proxy mv /etc/nginx/conf.d/proxy-${blue}.conf /etc/nginx/conf.d/proxy-${blue}
+docker exec node-hello-proxy mv /etc/nginx/conf.d/proxy-${green} /etc/nginx/conf.d/proxy-${green}.conf
+docker exec node-hello-proxy nginx -s reload
